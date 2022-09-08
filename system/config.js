11b15f1314963fa8aed63b07aa214285e5a40bd8
AMBYSBOTZ <106890463+AMBYSBOTZ@users.noreply.github.com>
2022-09-08 14:59:08 +0700

    config.js

--- system/config.js
+++ system/config.js
@@ -1,13 +1,13 @@
 // Owner number
-global.owner = '6281331265675'
+global.owner = '6285798267620'
 // Owner name
-global.owner_name = 'Reteam id'
-// Maximum upload file size limit (Default : 250 MB)
-global.max_upload = 250
+global.owner_name = 'XIAO SAN'
+// Maximum upload file size limit (Default : 500 MB)
+global.max_upload = 500
 // Delay for spamming protection (Default : 3 seconds)
 global.cooldown = 3
-// User Limitation (Default : 10)
-global.limit = 10
+// User Limitation (Default : 30)
+global.limit = 30
 // Time to be temporarily banned and others (Default : 30 minutes)
 global.timer = 1800000
 // Symbols that are excluded when adding a prefix (Don't change it)
@@ -15,7 +15,7 @@ global.evaluate_chars = ['=>', '~>', '<', '>', '$']
 // Country code that will be automatically blocked by the system, when sending messages in private chat
 global.blocks = ['91', '92', '212']
 // Put target jid to forward friends story
-global.forwards = '6285221100126@c.us'
+global.forwards = '6285798267620@c.us'
 // Get neoxr apikey by registering at https://api.nxr.my.id
 global.Api = new (require('./neoxrApi'))('2lleJx')
 // Get bid and key configuration for autoreply chat ai feature by registering at https://brainshop.ai
